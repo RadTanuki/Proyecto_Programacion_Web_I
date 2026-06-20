16ad9cea96d42f63b2e3e1939249b6c41561f4de
RodrigoMora1710 <romoraca@est.utn.ac.cr>
2026-06-20 17:54:15 -0600

    Creación del JS de calculoNora

---
+++ js/grade.js
@@ -0,0 +1,11 @@
+const botonCalcularNota = document.getElementById("calcularNota")
+const botonAgregarRubro = document.getElementById("agregarRubro")
+const botonLimpiar = document.getElementById("limpiar")
+const cuadroResultado = document.getElementById("result")
+
+function crearRubro() {
+
+    
+}
+
+
