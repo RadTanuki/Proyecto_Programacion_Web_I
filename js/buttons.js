5b3504c964af9be76ad26c3dfdef446678cb0017
RodrigoMora1710 <romoraca@est.utn.ac.cr>
2026-06-26 22:34:31 -0600

    Primeros pasos del modo oscuro

--- js/buttons.js
+++ js/buttons.js
@@ -43,4 +43,15 @@ function clearFilters(){
     applyFilters();
 }
 
+let toggle = document.getElementById('toggle');
+toggle.addEventListener('change', (event)=>{
+    let checked=event.target.checked;
+    document.body.classList.toggle('dark');
+
+
+
+})
+
+
+
 btnClearFilters.addEventListener('click', clearFilters);
