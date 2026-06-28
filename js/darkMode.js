34668e4317905caa1fdeea9e7c6b2d865687c11e
RodrigoMora1710 <romoraca@est.utn.ac.cr>
2026-06-27 20:08:51 -0600

    Implementación del modo oscuro en todas las paginas (sin local Storage todavía)

---
+++ js/darkMode.js
@@ -0,0 +1,19 @@
+/*Dark Mode*/
+
+const botonDark = document.getElementById("botonDark");
+
+botonDark.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+        botonDark.textContent = "\u{2600}";
+
+    } else {
+
+        botonDark.textContent = "\u{1F319}";
+
+    }
+
+});
